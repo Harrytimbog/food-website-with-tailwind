cc10181d1c0710858da478ffc1e3b7a34e8eda8a
timilehinarigbede <harriemannie@gmail.com>
2023-06-12 17:50:09 +0100

    Create Foods Section

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HeadlineCards = () => {
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
@@ -39,7 +37,9 @@ const HeadlineCards = () => {
       <div className="rounded-xl relative">
         {/* Overlay */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">We Deliver Desserts Too</p>
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
           <p className="px-2">Tasty Treats</p>
           <button className="border-white bg-white text-black mx-2 absolute bottom-4">
             Order Now
